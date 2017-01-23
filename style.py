d13b28541ebc05d02500be6261cc29ba89546f6a
Vbarrete <vbarrete@student.42.fr>
2017-01-23 10:15:54 +0100

    Update style.py

--- style.py
+++ style.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
+import os, sys
+
+l_fd = []
+if len(sys.argv) == 1:
+    print("Invalide Argument")
+    exit()
+for i in range(1, len(sys.argv)-1):
+    try:
+        l_fd.append(open())
