e6edfea1ebdef3927525c0bb1abb1f06f056561f
Barreteau Victor <vbarrete@student.42.fr>
2017-01-27 09:04:10 +0100

    Update patern.py

--- patern.py
+++ patern.py
@@ -3,6 +3,13 @@
 
 import re
 
+def find_var(files, class_name):
+    var_name = re.compile("(.*) =[^=]")
+    name = []
+    for i in files:
+        name += re.findall(var_name, i)
+    return name
+
 def find_class(files):
     class_name = re.compile("class (.*):")
     name = []
